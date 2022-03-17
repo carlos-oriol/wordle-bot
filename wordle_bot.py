b1c2688e6f534f5a20c5ca4c0d747ae348704c21
Arturo Olvera <arturoolvera@arturo-mbpr.local>
2022-03-16 17:26:44 -0700

    input

--- wordle_bot.py
+++ wordle_bot.py
@@ -3,20 +3,29 @@ import pandas as pd
 
 print("Welcome!")
 
-def main():
-    print("Are you ready for today's WORDLE?")
-    
-if __name__ == "__main__":
-    main()
-
 # play_wordle plays the wordle game using the dataset provided as a csv
 def play_wordle():
     words_df = pd.read_csv('five_letters.csv')
     attempt = 1
 
+    while True:
+        word = "terai"
+        print("I guess" + word)
+        print("result?")
+        x = input()
+        print(x)
     # Wordle allows only for 6 attempts (Initial Guess + 5)
-    while attempt <= 5:
-        
-        return None
+    #while attempt <= 5:
+    #    
+    #    return None
 
-    return None
+    #return None
+
+
+
+def main():
+    print("Are you ready for today's WORDLE?")
+    play_wordle()
+    
+if __name__ == "__main__":
+    main()
