441880896bb21ae816f2bfcffd1e9095d23c77ea
Carlos Oriol Lopez-Tenorio <carlosoriol@berkeley.edu>
2022-03-16 18:30:23 -0700

    Major advances

--- wordle_bot.py
+++ wordle_bot.py
@@ -1,30 +1,55 @@
 import sys
 import pandas as pd
 
-print("Welcome!")
-
 # play_wordle plays the wordle game using the dataset provided as a csv
 def play_wordle():
-    words_df = pd.read_csv('five_letters.csv')
+    words_df = pd.read_csv('/Users/carlosoriol/Desktop/Projects/WORDLE/worlde-bot/five_letters.csv')
     attempt = 1
 
-    while True:
-        word = "terai"
-        print("I guess" + word)
-        print("result?")
-        x = input()
-        print(x)
-    # Wordle allows only for 6 attempts (Initial Guess + 5)
-    #while attempt <= 5:
-    #    
-    #    return None
+    while attempt < 6:
+        
+        word = 'TERAI'
+        valid = False
 
-    #return None
+        # Initial Guess if attempt == 1
+        if attempt == 1:
+            print("My first guess is: " + word)
+        
+        print('---------------------'+ '\n')
+        print("What was the result?")
+        print('Please enter a 5 letter string with either Y for yellow, G for green, or X for grey.')
 
+        while True:
+            
+            result = input('Enter a word: ')
+
+            # Edge case for when the input is null
+            if result == '':
+                print('Please enter a string.')
+                continue
+
+            # Edge case for when the input is not 5 digits
+            if len(result) != 5:
+                print()
+                print('THIS STRING IS NOT VALID. TRY AGAIN!')
+                continue
+
+            else:
+                break
+
+        attempt += 1
+        print('There are ' + str(6 - 1) + ' attempts remaining.')
+
+    # Edge case for when attempts have run out
+    if attempt == 6:
+        print('We could not get it today. Try again tomorrow!')
+        return None
 
 
 def main():
-    print("Are you ready for today's WORDLE?")
+    print()
+    print('---------------------')
+    print("Welcome! Are you ready for today's WORDLE?")
     play_wordle()
     
 if __name__ == "__main__":
